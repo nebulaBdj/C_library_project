e299338527b321df28ff4f0c7ae3ba4c524fa797
Jeongin205 <ji801089@gmail.com>
2024-12-01 23:55:47 +0900

    feat: 회원가입 기능 기본 구조

--- auth.c
+++ auth.c
@@ -1,10 +1,40 @@
 #include "library.h"
 
 void sign_up(){
+	printf("\n");
+	print_menu_name("회원 가입");
+	printf("학번, 비밀번호, 이름, 주소, 전화번호를 입력하세요.\n\n");
+	ClientNode newClient = get_client_info();
+	printf("\n");
+	//정보에 학번 있나 체크
+	if(1){
+	// 불러온 정보에 끝에 붙이고
+	// 정렬해주기
+	// 파일에 저장하기
+		printf("회원가입이 되셨습니다.\n");
+	}else{
+		printf("이미 존재하는 학번입니다.\n");
+	}
+	printf("2초후 메인화면으로 돌아갑니다\n");
+	sleep(2);
+}
 
+ClientNode get_client_info(){
+	ClientNode newClient = (ClientNode)malloc(sizeof(Client));
+	printf("학번: ");
+	scanf("%d", &newClient->id);
+	getchar();
+
+	get_string("비밀번호: ", newClient->password);
+
+	get_string("이름: ", newClient->name);
+
+	get_string("주소: ", newClient->address);
+
+	get_string("전화번호: ", newClient->address);
+	return newClient;
 }
 
 int sign_in(){
-	
 	return 0;
 }
