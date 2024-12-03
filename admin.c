fb1ab3df4b474f2e5fbef6099b92ed669774463b
Jeongin205 <ji801089@gmail.com>
2024-12-04 01:19:16 +0900

    관리자 메뉴 및 회원 메뉴 기본 출력

--- admin.c
+++ admin.c
@@ -2,7 +2,43 @@
 
 void menu_admin(){
 	printf("\n");
-	print_menu_name("관리자 메뉴");
 	int num;
-	num = select_num();
+	do{
+		print_menu_name("관리자 메뉴");
+		printf("1. 도서 등록\t\t2. 도서 삭제\n");
+		printf("3. 도서 대여\t\t4. 도서 반납\n");
+		printf("5. 도서 검색\t\t6. 회원 목록\n");
+		printf("7. 로그아웃\t\t8. 프로그램 종료\n\n");
+		num = select_num();
+		switch(num){
+			case 1:
+				//도서 등록 
+				break;
+			case 2:
+				//도서 삭제
+				break;
+			case 3:
+				//도서 대여
+				break;
+			case 4:
+				//도서 반남
+				break;
+			case 5:
+				//도서 검색
+				break;
+			case 6:
+				//회원 목록
+				break;
+			case 7:
+				//로그아웃
+				break;
+			case 8:
+				//프로그램 종료
+				exit(1);
+			default:
+				printf("잘못된 번호입니다. 2초후 다시 입력하세요.\n");
+				sleep(2);
+		}
+		system("clear");
+	}while(num != 7);
 }
--- client.c
+++ client.c
@@ -1,10 +1,38 @@
 #include "library.h"
 
 void menu_client(){
-	int num;
-	// 루프 짜야함
 	printf("\n");
-	print_menu_name("회원 메뉴");
-	//메뉴 출력
-	num = select_num();
+	int num;
+	do{
+		print_menu_name("회원 메뉴");
+		printf("1. 도서 검색\t\t2. 내 대여 목록\n");
+		printf("3. 게인정보 수정\t4. 회원 탈퇴\n");
+		printf("5. 로그아웃\t\t6. 프로그램 종료\n\n");
+		num = select_num();
+		switch(num){
+			case 1:
+				//도서 검색 
+				break;
+			case 2:
+				//내 대여 목록
+				break;
+			case 3:
+				//개인정보 수정
+				break;
+			case 4:
+				//회원 탈퇴 
+				break;
+			case 5:
+				//로그아웃
+				break;
+			case 6:
+				//프로그램 종료
+				exit(1);
+				break;
+			default:
+				printf("잘못된 번호입니다. 2초후 다시 입력하세요.\n");
+				sleep(2);
+		}
+		system("clear");
+	}while(num != 5);
 }
